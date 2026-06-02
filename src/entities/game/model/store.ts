ba2d23e574205c325af11ab6a4b28cc975cb93da
Andrii Dmtryk <admitruk237@gmail.com>
2026-06-02 15:21:25 +0200

    refactor(entities): move zustand store into entities/counter slice per FSD

--- src/entities/game/model/store.ts
+++ src/entities/game/model/store.ts
@@ -1,12 +1,7 @@
 import { create } from "zustand";
+import type { CounterState } from "./types";
 
-interface AppState {
-  count: number;
-  increment: () => void;
-  decrement: () => void;
-}
-
-export const useAppStore = create<AppState>((set) => ({
+export const useCounterStore = create<CounterState>((set) => ({
   count: 0,
   increment: () => set((state) => ({ count: state.count + 1 })),
   decrement: () => set((state) => ({ count: state.count - 1 })),
