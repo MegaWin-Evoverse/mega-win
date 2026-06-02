737c3a15e90be33dad097bed9a7381d955b6b476
Andrii Dmtryk <admitruk237@gmail.com>
2026-06-02 15:19:04 +0200

    fix(shared/ui): fix setState-in-effect lint errors in carousel and use-mobile

--- src/shared/ui/carousel.tsx
+++ src/shared/ui/carousel.tsx
@@ -95,7 +95,7 @@ function Carousel({
 
   React.useEffect(() => {
     if (!api) return
-    onSelect(api)
+    api.on("init", onSelect)
     api.on("reInit", onSelect)
     api.on("select", onSelect)
 
