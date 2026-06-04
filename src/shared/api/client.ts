f618ca86ad90ec27cbf8bfa1d97ba5de6cca4631
knnseleznov <knnseleznov@gmail.com>
2026-06-04 19:19:44 +0100

    feat: add authApi axios instance

--- src/shared/api/client.ts
+++ src/shared/api/client.ts
@@ -1,5 +1,11 @@
 import axios from 'axios';
 
+export const authApi = axios.create({
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL ?? '/api',
   headers: {
