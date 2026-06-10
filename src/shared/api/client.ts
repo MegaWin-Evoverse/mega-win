ba3f2da2f34682d4cc7339abfc58d6f6e38c8120
knnseleznov <knnseleznov@gmail.com>
2026-06-10 12:26:51 +0100

    refactor: update client.ts with one instance

--- src/shared/api/client.ts
+++ src/shared/api/client.ts
@@ -1,13 +1,6 @@
 import axios from 'axios';
 import { applyInterceptors } from './interceptors';
 
-export const authApi = axios.create({
-  withCredentials: true,
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
-
 export const api = axios.create({
   withCredentials: true,
 });
